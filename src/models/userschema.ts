5d6e55d7f9d89a9d37d34d126104bb9e842d7844
oluchioraekwe <oluchi.oraekwe@decagon.dev>
2021-09-24 22:59:13 +0100

    user model

---
+++ src/models/userschema.ts
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+
+export interface IUser {
+  firstnme: string;
+  lastname: string;
+  email: string;
+  password: string;
+  gender: string;
+  role: string;
+  location: string;
+  teams: string[];
+  about: string;
+  isVerified: boolean;
+  img: string;
+  resetpasswordtoken: string;
+  resetpasswordexpires: Date;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female'],
+    },
+    location: {
+      type: String,
+    },
+    teams: {
+      type: Array,
+    },
+    about: {
+      type: String,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    img: {
+      type: Buffer,
+      contentType: String,
+    },
+    resetpasswordtoken: {
+      type: String,
+    },
+    resetpasswordexpires: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model('user', userSchema);
+export default User;
