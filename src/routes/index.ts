d9e5fce98ee601e993a3f24a634b4b164d58f1ae
Decagon <maximmajor4us@gmail.com>
2021-09-28 14:18:11 +0100

    login-Authentication-middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ import auth from "../middleware/auth"
 
 /* GET home page. */
 router.get('/', auth, function (req: any, res: Response) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express ' });
 });
 
 export default router;
