65f64d8d6781547737068ae870b0a06c04b9274c
Decagon <t@Folas-MacBook-Air.local>
2021-10-03 23:54:49 +0100

    Create task and file upload preliminary

--- src/models/tasksSchema.ts
+++ src/models/tasksSchema.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 export interface ITask {
   _id?: string,
   title: string,
+  projectID: string,
   assignedUser: string,
   description: string,
   files: [file],
@@ -18,10 +19,13 @@ export interface file {
 export interface comment {
   content : string
 }
-
 const taskSchema = new mongoose.Schema<ITask>(
   {
     projectID: {
+      type: String,
+      required: true
+    },
+    owner:{
       type: String,
       required: true
     },
@@ -48,7 +52,8 @@ const taskSchema = new mongoose.Schema<ITask>(
     },
     status: {
       type: String,
-      enum: ['backlog','todo', 'done']
+      enum: ['backlog','todo', 'done'],
+      default: "backlog"
     }
   },
   {
--- src/routes/profile-routes.ts
+++ src/routes/profile-routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { upload } from '../controller/file_uploads';
-import { create, Profile } from '../controller/profile';
+import { Profile } from '../controller/profile';
 import { showProfile } from '../controller/profile-controller';
+import { createTask} from '../controller/task';
 import { authCheck, isLoggedIn } from '../middleware/auth-check';
 const router = Router();
 
 router.get('/', isLoggedIn, showProfile);
-router.put('/edit',isLoggedIn,upload, Profile)
-router.post('/postUser', create)
+router.put('/edit',isLoggedIn,upload,Profile)
+router.post('/:projectId/task',isLoggedIn,upload,createTask)
+// router.delete('deleteTask', deleteTask )
+// router.post('/:projectId/task', createTask)
 
 export default router;
