badb9556ff3eb3f547cca0c61ba431278eea12e5
Ebuka <bukasin1@gmail.com>
2021-10-15 04:04:10 +0100

    login and profile refactored

--- dist/routes/auth-routes.js
+++ dist/routes/auth-routes.js
@@ -5,11 +5,11 @@ const auth_controller_1 = require("../controller/auth-controller");
 const router = (0, express_1.Router)();
 //LOGIN PAGE
 // router.post('/login', loginPage);
-router.post('/login', (0, auth_controller_1.localAuth)());
+router.post('/login', (0, auth_controller_1.localAuth)(), auth_controller_1.loginRedirect);
 router.get('/login', auth_controller_1.login);
-router.get('/google', auth_controller_1.googleHandler);
+router.get('/google', auth_controller_1.googleHandler, auth_controller_1.loginRedirect);
 router.get('/logout', auth_controller_1.logout);
-router.get('/google/redirect', auth_controller_1.redirectHandler, auth_controller_1.redirect);
+router.get('/google/redirect', auth_controller_1.redirectHandler, auth_controller_1.loginRedirect);
 router.get("/facebook", (0, auth_controller_1.getFacebookAuth)());
 router.get("/facebook/callback", (0, auth_controller_1.authFacebook)());
 exports.default = router;
